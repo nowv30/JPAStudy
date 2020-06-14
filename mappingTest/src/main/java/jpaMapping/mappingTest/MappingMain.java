7c19a39124e1a896f97572f3851bf00e900290ad
kowvic <52282781+kowvic@users.noreply.github.com>
2020-06-14 17:26:03 +0900

    단방향 연관관계 설정( 저장, 조회)

--- mappingTest/src/main/java/jpaMapping/mappingTest/MappingMain.java
+++ mappingTest/src/main/java/jpaMapping/mappingTest/MappingMain.java
@@ -17,26 +17,46 @@ public class MappingMain {
 		tx.begin();
 		
 		try {
+			//1. 객체를 테이블에 맞춰 모델링--------------
 			//팀과 멤버 정보를 넣는다
-			Team team = new Team();
-			team.setName("TeamA");
+//			Team team = new Team();
+//			team.setName("TeamA");
 			//persist실행하면서 id값 생성
-			em.persist(team);
+			//em.persist(team);
 			
 			//멤버정보를 넣으면서 팀에 소속시킨다.
 			//연관관계가 없어서 외레키 식별자를 직접 다룬다.
-			Member member = new Member();
-			member.setUsername("member1");
-			member.setTeamId(team.getId());
-			em.persist(member);
+//			Member member = new Member();
+//			member.setUsername("member1");
+//			member.setTeamId(team.getId());
+//			em.persist(member);
 			
 			//멤버를 찾아온다.
-			Member findMember = em.find(Member.class, member.getId());
+//			Member findMember = em.find(Member.class, member.getId());
 			
 			//멤버의 소속팀을 알고싶다.
 			//연관관계라는 게 없어서 DB에서 값을 계속 가져와야한다. 객체지향적이지 않다.
-			long findTeamId= findMember.getTeamId();
-			Team findTeam = em.find(Team.class, findTeamId);
+//			long findTeamId= findMember.getTeamId();
+//			Team findTeam = em.find(Team.class, findTeamId);
+			//----------------------------
+			
+			//2. 객체지향 모델링------------------------
+			//팀저장
+			Team team = new Team();
+			team.setName("TeamB");
+			em.persist(team);
+			
+			//회원저장
+			Member member = new Member();
+			member.setUsername("Member2");
+			member.setTeam(team);//단방향 연관관계 설정, 잠조 저장
+			
+			//회원조회
+			Member findMember = em.find(Member.class, member.getId());
+			//참조를 이용하여 회원의 팀 조회
+			Team findTeam= findMember.getTeam();
+			em.persist(findTeam);
+			System.out.println("findTeam="+findTeam.getName());
 			
 			tx.commit();
 			
--- mappingTest/src/main/java/jpaMapping/mappingTest/domain/Member.java
+++ mappingTest/src/main/java/jpaMapping/mappingTest/domain/Member.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -19,6 +21,13 @@ public class Member {
 	@Column(name="USER_NAME")
 	private String username;
 	
-	@Column(name="TEAM_ID")
-	private long teamId;
+//	@Column(name="TEAM_ID")
+//	private long teamId;
+	
+	//멤버입장에서Many, 팀입장에서One
+	//Many => One 방향
+	@ManyToOne
+	//Team테이블에 Id와 조인. team과 TEAM_ID왜래키와 연관관계 매핑
+	@JoinColumn(name="TEAM_ID")
+	private Team team;
 }
